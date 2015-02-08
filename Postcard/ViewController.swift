4ed4061bdd3f3b664b4c6c2166d59da4b0bf1edf
victor guillen <victor.guillen@gmail.com>
2015-02-08 04:47:52 -0400

    first ios lesson commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButton(sender: UIButton) {
+        //messageLabel.text = "Hello World"
+        //Accesing text in a text box
+        messageLabel.text = "Message:" + messageTextField.text
+        nameLabel.text = "To:" + nameTextField.text
+        
+        messageLabel.hidden = false
+        nameLabel.hidden = false
+        
+        messageLabel.textColor = UIColor.redColor()
+        nameLabel.textColor = UIColor.blueColor()
+        //Clean the text field
+        messageTextField.text = ""
+        nameTextField.text = ""
+        
+        
+        
+        // hide the keyboard
+        messageTextField.resignFirstResponder()
+        sendButton.setTitle("Message Send", forState: UIControlState.Normal)
+    }
 
 }
 
